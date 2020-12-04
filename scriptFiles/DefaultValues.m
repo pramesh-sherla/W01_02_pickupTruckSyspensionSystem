959a7b1d483d00c173fe2c91e3c62cb3963b9efe
Pramesh Sherla <pramesherla121@gmail.com>
2020-12-04 19:20:43 +0530

    Initial Commit. Simulink Model is created with Default Values.

---
+++ scriptFiles/DefaultValues.m
@@ -0,0 +1,9 @@
+%% Default values
+
+% Assumptions
+
+M1 = 500; % Mass of Vehicale
+m2 = 20;  % Mass of Wheel
+k1 = 250; % Spring Coefficient of Suspension
+b  = 1;   % Damping constant of Suspension
+k2 = 1;   % Spring Coefficient of Suspension
