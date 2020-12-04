3d3684f973880942db32c3c0ee3132566050fcbf
Pramesh Sherla <pramesherla121@gmail.com>
2020-12-04 20:59:33 +0530

    Updated Values. M1 = 500, m2 = 20, k1 = 500, k2 = 1, b=500

--- scriptFiles/DefaultValues.m
+++ scriptFiles/DefaultValues.m
@@ -4,8 +4,8 @@
 
 M1 = 500; % Mass of Vehicale
 m2 = 20;  % Mass of Wheel
-k1 = 250; % Spring Coefficient of Suspension
-b  = 1;   % Damping constant of Suspension
+k1 = 500; % Spring Coefficient of Suspension
+b  = 500;   % Damping constant of Suspension
 k2 = 1;   % Spring Coefficient of Suspension
 
 %% Road Equation
