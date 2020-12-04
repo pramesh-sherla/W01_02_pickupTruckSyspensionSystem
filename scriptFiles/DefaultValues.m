036732e712cadbed0c89599ee716354ff7483aa8
Pramesh Sherla <pramesherla121@gmail.com>
2020-12-04 19:50:22 +0530

    Updated Script File And Model. In script file a signal with addition of sine wave and noise is added to emulate road parameter of bumps and potholes. Also to get the signal from Matlab WorksSpace to Simulink model a input post is added to the model.

--- scriptFiles/DefaultValues.m
+++ scriptFiles/DefaultValues.m
@@ -7,3 +7,19 @@ m2 = 20;  % Mass of Wheel
 k1 = 250; % Spring Coefficient of Suspension
 b  = 1;   % Damping constant of Suspension
 k2 = 1;   % Spring Coefficient of Suspension
+
+%% Road Equation
+
+h2r = 2 * pi;
+Fs = 48000;
+tStop = 1;
+dt = 1/Fs;
+t = (0:dt:tStop)';
+nFreq = 50;
+sigFreq = 1;
+%%
+k =0.05;
+sig1 = sin(sigFreq * t *h2r);
+noise =k*sin(nFreq * t *h2r);
+
+signal = sig1 + noise;
